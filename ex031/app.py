e2f1f2f70e77f5752877cbd3da749f307ea21bb7
Emerson Willian <emersonwdias2013@gmail.com>
2024-08-23 18:55:50 -0300

    modifique

--- ex031/app.py
+++ ex031/app.py
@@ -1,4 +1,4 @@
-viagem = float(input('Qual a distancia da vigem em km? '))
+viagem = float(input('Qual a distancia da viagem em km? '))
 print('Você esta preste a começar um viagem de {}km'.format(viagem))
 viagem_curta = viagem * 0.50
 viagem_longa =  viagem * 0.45
